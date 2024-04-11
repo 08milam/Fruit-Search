92b7533bcacf656c5516ec091eeb5c33414e0b5f
Matthew Milam <96691690+08milam@users.noreply.github.com>
2024-04-11 09:59:07 -0500

    update all

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Fruit-Search
-find the fruit you like
+Discover the future of fruit selection with our Predictive Fruit Search. Simply start typing your desired fruit, and let our advanced algorithms suggest personalized options based on your taste preferences and nutritional needs. Say goodbye to guesswork and hello to perfect fruit every time!
+
+![fruit-search-image](./fruit-search-image.png)
+
+HOW TO RUN 
+------------------------------------
+* To get started, navigate to the Fruit-Search repository on GitHub and download the files. Then, open the folder in Visual Studio Code and launch the live server extension. This action  will automatically open the project in your default web browser.
+
+TECHNOLOGY STACK USED
+------------------------------------
+* HTML
+* CSS
+* JavaScript
+
---
+++ script.js
@@ -0,0 +1,128 @@
+
+// title: autocomplete.js
+// type: js
+// creation date: 11/7/2023
+// modification date: date:12/30/23
+// note: 
+// programmer: Matthew Milam
+
+const input = document.querySelector("#fruit");
+const suggestions = document.querySelector(".suggestions ul");
+
+const fruit = [
+  "Apple",
+  "Apricot",
+  "Avocado",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu",
+];
+
+function toObject(fruit) {
+  var rv = {};
+  for (var i = 0; i < fruit.length; ++i)
+    if (fruit[i] !== undefined) rv[i] = fruit[i];
+
+  return rv;
+};
+
+let newlist = toObject(fruit);
+
+input.addEventListener("input", (e) => {
+  suggestions.replaceChildren();
+    let autocomplete = [];
+    let inputbox = input.value;
+    //make a filter for input and convert all to lower case
+  if (inputbox) {
+    autocomplete = fruit.filter(function (keyword) {
+      return keyword.toLowerCase().includes(inputbox.toLowerCase());
+    });
+// loops over the list and gets to value
+    for (let i = 0; i < autocomplete.length; i++) {
+      let answer = autocomplete[i];
+  //create the li
+      let useSuggestion = document.createElement("li");
+  //targets the click itme
+      useSuggestion.addEventListener('click', function(e){
+        const selectItem = e.target;
+       clickautofill = selectItem.innerText;
+       input.value = clickautofill;
+      });
+      useSuggestion.innerText = answer;
+      suggestions.appendChild(useSuggestion);
+    }
+  }
+});
